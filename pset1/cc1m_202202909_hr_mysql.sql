d12ab59988b55e563adbfa18c9ccbeb77dc3fcbf
aura <111578898+aurahtml@users.noreply.github.com>
2022-11-07 20:36:31 -0300

    Update cc1m_202202909_hr_mysql.sql

--- pset1/cc1m_202202909_hr_mysql.sql
+++ pset1/cc1m_202202909_hr_mysql.sql
@@ -149,10 +149,6 @@ CREATE UNIQUE INDEX empregados_idx
  ON empregados
  ( email );
 
-CREATE UNIQUE INDEX empregados_idx1
- ON empregados
- ( email );
-
 CREATE TABLE gerentes (
                 id_gerente INT NOT NULL,
                 id_departamento INT NOT NULL,
